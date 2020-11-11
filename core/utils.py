94752da1543ebfb49a65824ca54bee01bacf8440
joao.monte <joao.monte@gprt.ufpe.br>
2020-11-11 20:30:20 -0300

    Added a file with helper functions

---
+++ core/utils.py
@@ -0,0 +1,8 @@
+import python_jwt as jwt
+
+#Helper functions
+
+def generateToken(email, password, key):
+    user_info = {"email": email, "password": password }
+    return jwt.generate_jwt(user_info, key, 'HS256', datetime.timedelta(minutes=10))
+    
