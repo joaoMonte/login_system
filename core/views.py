2fd8b068d9642182d845f496df794dd4d8f44208
joao.monte <joao.monte@gprt.ufpe.br>
2020-11-11 20:21:52 -0300

    Added the token generation for signup view

--- core/views.py
+++ core/views.py
@@ -1,14 +1,17 @@
 import json
 
+import python_jwt as jwt
+import jwcrypto.jwk as jwk
+import datetime
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import User, Phone
 
-
 # Create your views here.
 @csrf_exempt
 def signup(request):
+
     if request.method == "POST":
         user_json = json.loads(request.body.decode('UTF-8'))
         if User.objects.filter(email=user_json["email"]).count() > 0:
@@ -30,19 +33,28 @@ def signup(request):
             new_user.save()
             new_phone.save()
 
-            response = {"Sucess": "User created!"}
+            if "key_json" not in request.session:
+                request.session["key_json"] = jwk.JWK.generate(kty='oct', size=256).export()
+            
+            key = jwk.JWK.from_json(request.session["key_json"])
+            user_info = {"email": user_json["email"], "password": user_json["password"] }
+            token = jwt.generate_jwt(user_info, key, 'HS256', datetime.timedelta(minutes=10))
+            response = {"token": token}
     else:
         response = {"Error": "Invalid method"}
     return JsonResponse(response)
 
 @csrf_exempt
 def signin(request):
+    print(key)
+
     if request.method == "POST":
         login_information = json.loads(request.body.decode('UTF-8'))
         email = login_information["email"]
         password = login_information["password"]
         user = User.objects.filter(email=email)
         if user.count() == 1 and user.first().password == password:
+
             response = {"Sucess": "Login sucessfully"}
         else:
             response = {"Error": "Invalid e-mail or password"}
@@ -52,6 +64,8 @@ def signin(request):
 
 @csrf_exempt
 def me(request):
+    print(key)
+
     if request.method == "GET":
         user = User.objects.get(email=email)
         phones = Phone.objects.get(ownerEmail=email)
