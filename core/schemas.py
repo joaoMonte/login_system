6a1764417afa91ff2d98761bddbedaa5b3367f22
joao.monte <joao.monte@gprt.ufpe.br>
2020-11-12 10:28:36 -0300

    Added the patterns for strings

--- core/schemas.py
+++ core/schemas.py
@@ -2,18 +2,30 @@ signup_schema = {
     "title": "The schema for signup",
     "type": "object",
     "properties": {
-        "firstName": {"type": "string"},
-        "lastName": {"type": "string"},
-        "email": {"type": "string"},
-        "password": {"type": "string"},
+        "firstName": {"type": "string", "minLength": 1},
+        "lastName": {"type": "string", "minLength": 1},
+        "email": {
+            "type": "string",
+            "pattern": "^[a-z0-9._]+[@]{1}[a-z0-9_]+[.]{1}[a-z0-9_]+$"
+        }
+        "password": {"type": "string", "minLength": 1},
         "phones": {
             "type": "array",
             "items": {
                 "type": "object",
                 "properties": {
-                    "number": {"type": "string"},
-                    "area_code": {"type": "string"},
-                    "country_code": {"type": "string"}
+                    "number": {
+                        "type": "string",
+                       "pattern": "^[0-9]+$"
+                    },
+                    "area_code": {
+                        "type": "string"
+                        "pattern": "^[0-9]+$"
+                    },
+                    "country_code": {
+                        "type": "string",
+                        "pattern": "\\+[0-9]{2}"
+                    }
                 },
                 "required": ["number", "area_code", "country_code"],
                 "additionalProperties": False,
@@ -29,8 +41,11 @@ signin_schema = {
     "title": "The schema for signin",
     "type": "object",
     "properties": {
-        "email": {"type": "string"},
-        "password": {"type": "string"},
+        "email": {
+            "type": "string",
+            "pattern": "^[a-z0-9._]+[@]{1}[a-z0-9_]+[.]{1}[a-z0-9_]+$"
+        },
+        "password": {"type": "string", "minLength": 1},
     }
     "required": ["email", "password"],
     "additionalProperties": False,
