8d338087ce8915aa4c6a47c9c1922a70f93ee728
joao.monte <joao.monte@gprt.ufpe.br>
2020-11-12 12:02:20 -0300

    Implemented the class for signup and the first test

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+import json
 
-# Create your tests here.
+
+class TestSignup(TestCase):
+
+    def test_insert_correct_user(self):
+        new_user = {
+            "firstName": "joao",
+            "lastName": "monte",
+            "email": "joao@joao.com",
+            "password": "helloworld",
+            "phones": [
+                {
+                    "number": "12345678",
+                    "area_code": "81",
+                    "country_code": "+55"
+                },
+                {
+                    "number": "87654321",
+                    "area_code": "81",
+                    "country_code": "+55"
+                }
+            ]
+        }
+        response = self.client.post(reverse("signup"), json.dumps(new_user), content_type="application/json")
+        assert response.status_code == 200
+        assert "token" in response.json()
+
+
+
