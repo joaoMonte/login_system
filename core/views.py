059a6e67b6847ec50e3add2d2a16ec7cf547814f
joao.monte <joao.monte@gprt.ufpe.br>
2020-11-12 10:49:39 -0300

    Added validation error coverage and fixed a bug: Only the last number was being saved for a user

--- core/views.py
+++ core/views.py
@@ -7,6 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import User, Phone
 from .utils import generateToken
+from .schemas import signin_schema, signup_schema
+from jsonschema import validate, ValidationError
 
 # Create your views here.
 @csrf_exempt
@@ -14,31 +16,40 @@ def signup(request):
 
     if request.method == "POST":
         user_json = json.loads(request.body.decode('UTF-8'))
-        if User.objects.filter(email=user_json["email"]).count() > 0:
-            response = {"Error": "E-mail already exists"}
-        else:
-            new_user = User(
-                firstName = user_json["firstName"],
-                lastName = user_json["lastName"],
-                email = user_json["email"],
-                password = user_json["password"]
-            )
-            for phone in user_json["phones"]:
-                new_phone = Phone(
-                    ownerEmail = user_json["email"],
-                    number = user_json["phones"]["number"],
-                    area_code = user_json["phones"]["area_code"],
-                    country_code = user_json["phones"]["country_code"]
-                )
-            new_user.save()
-            new_phone.save()
+        try:
+            validate(user_json, schema=signup_schema)
 
-            if "key_json" not in request.session:
-                request.session["key_json"] = jwk.JWK.generate(kty='oct', size=256).export()
-            
-            key = jwk.JWK.from_json(request.session["key_json"])
-            token = generateToken(new_user.email, new_user.password, key)
-            response = {"token": token}
+            if User.objects.filter(email=user_json["email"]).count() > 0:
+                response = {"Error": "E-mail already exists"}
+            else:
+                new_user = User(
+                    firstName = user_json["firstName"],
+                    lastName = user_json["lastName"],
+                    email = user_json["email"],
+                    password = user_json["password"]
+                )
+                new_user.save()
+
+                for phone in user_json["phones"]:
+
+                    new_phone = Phone(
+                        ownerEmail = user_json["email"],
+                        number = phone["number"],
+                        area_code = phone["area_code"],
+                        country_code = phone["country_code"]
+                    )
+                    new_phone.save()
+
+
+                if "key_json" not in request.session:
+                    request.session["key_json"] = jwk.JWK.generate(kty='oct', size=256).export()
+                
+                key = jwk.JWK.from_json(request.session["key_json"])
+                token = generateToken(new_user.email, new_user.password, key)
+                response = {"token": token}
+        except ValidationError as err:
+            response = {"Validation Error" : str(err)}
+
     else:
         response = {"Error": "Invalid method"}
     return JsonResponse(response)
@@ -47,16 +58,21 @@ def signup(request):
 def signin(request):
 
     if request.method == "POST":
-        login_information = json.loads(request.body.decode('UTF-8'))
-        email = login_information["email"]
-        password = login_information["password"]
-        user = User.objects.filter(email=email)
-        if user.count() == 1 and user.first().password == password:
-            key = jwk.JWK.from_json(request.session["key_json"])
-            token = generateToken(email, password, key)
-            response = {"token": token}
-        else:
-            response = {"Error": "Invalid e-mail or password"}
+        try:
+            login_information = json.loads(request.body.decode('UTF-8'))
+            validate(login_information, signin_schema)
+
+            email = login_information["email"]
+            password = login_information["password"]
+            user = User.objects.filter(email=email)
+            if user.count() == 1 and user.first().password == password:
+                key = jwk.JWK.from_json(request.session["key_json"])
+                token = generateToken(email, password, key)
+                response = {"token": token}
+            else:
+                response = {"Error": "Invalid e-mail or password"}
+        except ValidationError as err:
+            response = {"Validation Error" : str(err)}
     else:
         response = {"Error": "Invalid method"}
     return JsonResponse(response)
