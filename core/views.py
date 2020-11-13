c463a796c4efadb03303e64232184bfe0fe8984e
joao.monte <joao.monte@gprt.ufpe.br>
2020-11-13 16:35:01 -0300

    Added comments and a new exception on view /me

--- core/views.py
+++ core/views.py
@@ -13,15 +13,18 @@ from jsonschema import validate, ValidationError
 # Create your views here.
 @csrf_exempt
 def signup(request):
-
+    #This view only accepts POST requests
     if request.method == "POST":
         user_json = json.loads(request.body.decode('UTF-8'))
         try:
+            #Validate to check if all fields in body are right
             validate(user_json, schema=signup_schema)
 
+            #Check if the email already exists on database
             if User.objects.filter(email=user_json["email"]).count() > 0:
                 response = {"Error": "E-mail already exists"}
             else:
+                #Register the user and his phones
                 new_user = User(
                     firstName = user_json["firstName"],
                     lastName = user_json["lastName"],
@@ -39,17 +42,17 @@ def signup(request):
                         country_code = phone["country_code"]
                     )
                     new_phone.save()
-
-
+                #If the key for this session hasn't been generated, do it. This key will
+                #be used to generate and verify jwts while this session is active
                 if "key_json" not in request.session:
                     request.session["key_json"] = jwk.JWK.generate(kty='oct', size=256).export()
                 
+                #Generate the JWT token using the key, and give it as response
                 key = jwk.JWK.from_json(request.session["key_json"])
                 token = generateToken(new_user.email, new_user.password, key)
                 response = {"token": token}
         except ValidationError as err:
             response = {"Validation Error" : str(err)}
-
     else:
         response = {"Error": "Invalid method"}
     return JsonResponse(response)
@@ -58,14 +61,19 @@ def signup(request):
 def signin(request):
 
     if request.method == "POST":
+        #This view only accepts POST requests
         try:
+            #Validate to check if all fields in body are right
             login_information = json.loads(request.body.decode('UTF-8'))
             validate(login_information, signin_schema)
 
+            #Check if the email and password are corrects
             email = login_information["email"]
             password = login_information["password"]
             user = User.objects.filter(email=email)
             if user.count() == 1 and user.first().password == password:
+                #if yes, generate a JWT token using the key and give it 
+                # as response for the request.
                 if "key_json" not in request.session:
                     request.session["key_json"] = jwk.JWK.generate(kty='oct', size=256).export()
                 key = jwk.JWK.from_json(request.session["key_json"])
@@ -82,17 +90,21 @@ def signin(request):
 @csrf_exempt
 def me(request):
     if request.method == "GET":
+        #This endpoint only accept GET requests
+        #The request only will be processed if the header "Authorization" 
+        # is in the request.  
         if 'Authorization' in request.headers:
             
             token = request.headers['Authorization']
             if "key_json" not in request.session:
                     request.session["key_json"] = jwk.JWK.generate(kty='oct', size=256).export()
-
+            #Using the key, the JWT token is verified
             key = jwk.JWK.from_json(request.session["key_json"])
             try:
                 data = jwt.verify_jwt(token, key, ['HS256'])[1]
                 email = data["email"]
-                
+                #Using the email recovered from JWT, the data from this
+                # specific user is searched and given as response
                 user = User.objects.get(email=email)
                 phones = Phone.objects.filter(ownerEmail=email)
                 response = {
@@ -109,7 +121,9 @@ def me(request):
                     }
                     response["phones"].append(phone_json)
             except jwt._JWTError:
-                response = {"Error": "Unauthorized - invalid session"}    
+                response = {"Error": "Unauthorized - invalid session"}
+            except KeyError:
+                response = {"Error": "Invalid JWT"}    
         else:
             response = {"Error": "Unauthorized"}    
     else:
