a64d04cd0d004a6a8a466d45ae0b0b374e56d933
joao.monte <joao.monte@gprt.ufpe.br>
2020-11-13 16:24:20 -0300

    Added a new test to check the insertion of an user with a email already registered

--- core/tests/test_views_signup.py
+++ core/tests/test_views_signup.py
@@ -5,6 +5,24 @@ import json
 
 
 class TestSignup(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        new_user = User(firstName = "other",
+                        lastName  = "user",
+                        email     = "other.user@joao.com",
+                        password  = "helloworld")
+        new_user.save()
+
+        new_phone1 = Phone(ownerEmail   = "other.user@joao.com",
+                           number       = "12345678",
+                           area_code    = "81",
+                           country_code = "+55")
+        new_phone2 = Phone(ownerEmail   = "other.user@joao.com",
+                           number       = "87654321",
+                           area_code    = "81",
+                           country_code = "+55")
+        new_phone1.save()
+        new_phone2.save()
 
     def test_insert_correct_user(self):
         new_user = {
@@ -36,6 +54,32 @@ class TestSignup(TestCase):
         assert user.lastName == new_user["lastName"] 
         assert user.password == new_user["password"]
         assert phones.count() == 2
+
+    def test_insert_user_email_existent(self):
+        new_user = {
+            "firstName": "joao",
+            "lastName": "monte",
+            "email": "other.user@joao.com",
+            "password": "helloworld",
+            "phones": [
+                {
+                    "number": "12345678",
+                    "area_code": "81",
+                    "country_code": "+55"
+                },
+                {
+                    "number": "87654321",
+                    "area_code": "81",
+                    "country_code": "+55"
+                }
+            ]
+        }
+        response = self.client.post(reverse("signup"), 
+                                    json.dumps(new_user),
+                                    content_type="application/json")
+        
+        assert response.json() == {"Error": "E-mail already exists"}
+
 
     def test_insert_user_empty_name(self):
         new_user = {
